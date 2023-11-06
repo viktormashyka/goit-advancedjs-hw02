d5c46d520ce33f7882d58cd5367428d8740f094f
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-11-06 08:32:52 +0000

    Deploying to gh-pages from @ viktormashyka/goit-advancedjs-hw02@b94e35c57a231aa7a1879e3db39416fc7ed0d27f 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */document.querySelector("button[data-start]");document.querySelector("button[data-stop]");
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function n(){return Math.floor(Math.random()*16777215).toString(16).padStart(6,0)}const e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]"),o=document.querySelector("body");let t=null;e.addEventListener("click",()=>{t||(t=setInterval(l,1e3))});r.addEventListener("click",a);function l(){o.style.backgroundColor=`#${n()}`}function a(){clearInterval(t),o.style.backgroundColor="",t=null}
 //# sourceMappingURL=commonHelpers.js.map
