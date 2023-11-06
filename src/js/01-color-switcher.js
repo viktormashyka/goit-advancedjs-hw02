b94e35c57a231aa7a1879e3db39416fc7ed0d27f
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-11-06 08:31:29 +0000

    completetask #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,30 @@
+function getRandomHexColor() {
+  return Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0);
+}
+
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+const bgColor = document.querySelector('body');
+
+let timerId = null;
+
+const onClickStart = startBtn.addEventListener('click', () => {
+  if (timerId) {
+    return;
+  }
+  timerId = setInterval(handleClickStart, 1000);
+});
+
+const onClickStop = stopBtn.addEventListener('click', handleClickStop);
+
+function handleClickStart() {
+  const color = (bgColor.style.backgroundColor = `#${getRandomHexColor()}`);
+}
+
+function handleClickStop() {
+  clearInterval(timerId);
+  bgColor.style.backgroundColor = '';
+  timerId = null;
+}
